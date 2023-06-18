fdba04c8f9a6a2e70ed46742269a123c8df2390f
niki <nikiwone@gmail.com>
2023-06-18 12:33:46 +0300

    added exam-prep 1

--- 10.exam-preparation/petstagram/src/index.js
+++ 10.exam-preparation/petstagram/src/index.js
@@ -1,1 +1,30 @@
-console.log('server start');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { auth } = require('./middlewares/authMiddleware');
+
+const routes = require('./routes');
+const PORT = 3000;
+
+const app = express();
+
+// TODO: change db name
+mongoose.connect(`mongodb://127.0.0.1:27017/petstagram`)
+    .then(() => console.log('DB Connected successfully'))
+    .catch(err => console.log('DB Error, ', err.message));
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(auth);
+app.use(routes);
+
+app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
---
+++ 10.exam-preparation/petstagram/src/managers/userManager.js
@@ -0,0 +1,36 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt');
+const { SECRET } = require('../config/config');
+
+exports.login = async (username, password) => {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        throw new Error('Invalid user or password!');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+        throw new Error('Invalid user or password!');
+    }
+
+    const payload = {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+    };
+
+    const token = await jwt.sign(payload, SECRET, {expiresIn: '2d'});
+    return token;
+};
+
+exports.register = async (userData) => {
+    const user = await User.findOne({username: userData.username});
+if (user) {
+    throw new Error('Username already exists!');
+}
+
+return User.create(userData);
+
+}
