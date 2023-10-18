24ef31b4eba3230c996d8dbf667a514bd400a9c7
niki <nikiwone@gmail.com>
2023-10-18 22:21:11 +0300

    CUB-P1 fixed handlebars triple curly braces

--- JS-Back-End-September-2023/04.Workshop-ExpressJS-and-Templating/src/index.js
+++ JS-Back-End-September-2023/04.Workshop-ExpressJS-and-Templating/src/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
-app.engine("hbs", handlebars.engine({extname: "hbs"}));
+// Handlebars configuration
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
 app.set("view engine", "hbs");
 app.set("views", "src/views");
+
+// Setup static files
+const staticFiles = express.static(path.resolve(__dirname, "public"));
+app.use(staticFiles);
 
 app.get("/", (req, res) => {
     res.render("index");
